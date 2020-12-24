fafae03dd1a9ac2d1a788d8fea53b037082d85b2
IsaiBuzany117 <72050808+IsaiBuzany117@users.noreply.github.com>
2020-12-23 21:00:12 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'index.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html'
+        })
+    ]
 }
